ea9191e43916ccbfda67bd6f3728adc018ad977f
brad barden <iamb@cpan.org>
2012-06-13 20:42:02 +0000

    Import of IAMB/Acme-IAMB-0.02 from CPAN.
    gitpan-cpan-distribution: Acme-IAMB
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         IAMB/Acme-IAMB-0.02.tar.gz
    gitpan-cpan-author:       IAMB
    gitpan-cpan-maturity:     released

--- lib/Acme/IAMB.pm
+++ lib/Acme/IAMB.pm
@@ -10,11 +10,11 @@ Acme::IAMB - The great new Acme::IAMB!
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 
 =head1 SYNOPSIS
@@ -48,6 +48,7 @@ sub function1 {
 
 sub function2 {
 }
+foobar
 
 =head1 AUTHOR
 
